c2bbd4241f7fc126d24273baf64e6b663e5deacb
Никита Попов <recoilvrn@gmail.com>
2024-04-22 23:21:45 +0300

    [+] Создал CustLinkedList [!] Перевел InMemoryHistoryManager на CustLinkedList

--- src/ru/practicum/service/InMemoryHistoryManager.java
+++ src/ru/practicum/service/InMemoryHistoryManager.java
@@ -1,36 +1,26 @@
 package ru.practicum.service;
 
 import ru.practicum.model.Task;
+import ru.practicum.utils.CustLinkedList;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final HashMap<Integer, Task> history;
 
-    public InMemoryHistoryManager() {
-        this.history = new LinkedHashMap<>();
-    }
+    CustLinkedList myLinkedList = new CustLinkedList();
 
     @Override
     public void add(Task task) {
-        if (task != null) {
-            if (this.history.containsKey(task.getId())){
-                remove(task.getId());
-            }
-            this.history.put(task.getId(), task);
-        }
-    }
-
-    @Override
-    public void remove(int id) {
-        this.history.remove(id);
+        myLinkedList.linkLast(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return new LinkedList<>(history.values());
+        return myLinkedList.getTasks();
+    }
+
+    @Override
+    public void remove(int id) {
+        myLinkedList.removeNode(id);
     }
 }
